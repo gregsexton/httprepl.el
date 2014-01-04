1b1a7e85ec23328d14c810549215e97914ef98fc
Greg Sexton <gregsexton@gmail.com>
2014-01-04 12:38:22 +0000

    Print a prompt

--- restrepl.el
+++ restrepl.el
@@ -7,7 +7,7 @@
 ;; Provides an interactive interface for making HTTP
 ;; requests. Inspiration was drawn from looking at the ielm
 ;; source. comint.el and request.el do much of the heavy lifting and
-;; their respective variables will control much of the behaviour of
+;; their respective variables will control some of the behaviour of
 ;; restrepl.
 
 ;;; Code:
@@ -39,13 +39,14 @@
   (unless (comint-check-proc (current-buffer))
     (let ((process (start-process "restrepl" (current-buffer) "hexl")))
       (set-process-query-on-exit-flag process nil)
+      ;; (insert restrepl-header)
       (goto-char (point-max))
-      (insert restrepl-header)
       (unless comint-use-prompt-regexp
         (let ((inhibit-read-only t))
           (add-text-properties
            (point-min) (point-max)
-           '(rear-nonsticky t field output inhibit-line-move-field-capture t)))))))
+           '(rear-nonsticky t field output inhibit-line-move-field-capture t)))
+        (comint-output-filter process restrepl-prompt)))))
 
 (defun restrepl ()
   "TODO"
